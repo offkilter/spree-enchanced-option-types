e10283420961ece1cbb165fc83eb47c68002a3a3
offkilter <thom@offkilterdesign.com>
2010-07-05 18:33:22 -0400

    Changed option value to decimal 10,2 from 10,0 to support cents Changed variant price calculation from usign to_i to to_f to stop truncating cents on calcuated prices.

--- db/migrate/20090929210740_add_amount_to_option_value.rb
+++ db/migrate/20090929210740_add_amount_to_option_value.rb
@@ -1,6 +1,6 @@
 class AddAmountToOptionValue < ActiveRecord::Migration
   def self.up
-    add_column :option_values, :amount, :decimal
+    add_column :option_values, :amount, :decimal, :precision => 10, :scale => 2
   end
 
   def self.down
