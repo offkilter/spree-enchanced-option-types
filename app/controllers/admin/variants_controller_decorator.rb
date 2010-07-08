1c2a65d20c338b4c5d0d3f8398b3b92a794c8bad
offkilter <thom@offkilterdesign.com>
2010-07-07 20:53:20 -0400

    Fixed regenerate to correctly delete all variants for product

--- app/controllers/admin/variants_controller_decorator.rb
+++ app/controllers/admin/variants_controller_decorator.rb
@@ -1,7 +1,10 @@
 Admin::VariantsController.class_eval do
   def regenerate
     product = Product.find_by_permalink(params[:product_id])
-    product.variants.delete_all
+    product.variants.each do |v|
+      v.deleted_at = Time.now()
+      v.save
+    end
     product.do_create_variants(:recreate)
     redirect_to :action => :index, :product_id => params[:product_id]
   end
--- app/models/option_value_decorator.rb
+++ app/models/option_value_decorator.rb
@@ -4,6 +4,8 @@ OptionValue.class_eval do
   after_update :adjust_variant_prices, :if => :amount_changed?
 
   def adjust_variant_prices
-    variants.each{|v| v.update_attribute(:price, v.calculate_price)}
+    
+      variants.each{|v| v.update_attribute(:price, v.calculate_price)}
+  
   end
 end
